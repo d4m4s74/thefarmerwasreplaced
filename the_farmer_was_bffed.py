713ee883b6d89f7d80fbae39c6469113b906617c
d4m4s74 <elmagique@gmail.com>
2025-11-01 16:47:08 +0100

    updated comments I forgot to change

--- the_farmer_was_bffed.py
+++ the_farmer_was_bffed.py
@@ -230,7 +230,7 @@ def the_farmer_was_brainfucked(code):
 # [>+>+<<-]                    # move value to helper cells 1 and 2
 # >>[<<+>>-]                   # move value back to value cell
 # <-------------------         # subtract test number (19) from value cell
-# [>-<[-]]  # if the value is not 0 revert value and pre negate the test flag
+# [>-<[-]]                     # if the value is not 0 pre negate the test flag
 # >+                           # set flag to indicate loop
 # [>>.>.<<<<<[-]>>[-]]         # print cell 6 (harvest) and 7 (plant) clear value then reset flag
 # <<                           # return to value cell
@@ -241,7 +241,7 @@ def the_farmer_was_brainfucked(code):
 # [>+>+<<-]                    # move value to helper cells 1 and 2
 # >>[<<+>>-]                   # move value back to value cell
 # <----                        # subtract test number (4) from value cell
-# [>-<[-]]                 # if the value is not 0 revert value and pre negate the test flag
+# [>-<[-]]                     # if the value is not 0 pre negate the test flag
 # >+                           # set flag to indicate loop
 # [>.>>.<<<<<[-]>>[-]]         # print cell 5 (till) and 7 (plant) clear value then reset flag
 # <<                           # return to value cell
@@ -251,7 +251,7 @@ def the_farmer_was_brainfucked(code):
 # [>+>+<<-]                    # move value to helper cells 1 and 2
 # >>[<<+>>-]                   # move value back to value cell
 # <-----                       # subtract test number (5) from value cell
-# [>-<[-]]                # if the value is not 0 revert value and pre negate the test flag
+# [>-<[-]]                     # if the value is not 0 pre negate the test flag
 # >+                           # set flag to indicate loop
 # [>.>>.<<<<<[-]>>[-]]         # print cell 5 (till) and 7 (plant) clear value then reset flag
 # <<                           # return to value cell and end elif
