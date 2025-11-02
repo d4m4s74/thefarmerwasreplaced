d827d567b516562fa857f3fb4028d6c6dfe4ca1a
d4m4s74 <elmagique@gmail.com>
2025-11-02 14:22:58 +0100

    Enhance plant info logic for hedge and empty tiles
    POTENTIAL BREAKING CHANGE
    moved water value left to make space for maze and dinosaurs

--- the_farmer_was_bffed.py
+++ the_farmer_was_bffed.py
@@ -16,7 +16,7 @@ set_world_size(8)
 #
 # Read:
 # first:
-# 0wwpppth (water level (0-.25, .25-.5, .5-.75, .75-1), plant type, tilled state, harvestable)
+# wwppppth (water level (0-.25, .25-.5, .5-.75, .75-1), plant type, tilled state, harvestable)
 # second if companion:
 # 00000xxx (plant)
 # third if companion:
@@ -43,7 +43,15 @@ set_world_size(8)
 # xxxxxxxx (counting 0 to 255 for every unique pumpkin, south)
 # sixth if measure pumpkin
 # xxxxxxxx (counting 0 to 255 for every unique pumpkin, west)
- 
+# second if measure hedge (maze)
+# 00xxxxxx (X)
+# third if measure hedge (maze)
+# 00xxxxxx (Y)
+# fourth if measure hedge (maze)
+# 0000xxxx (available moves bitmask) (North=1, East=2, South=4, West=8)
+# second if measure empty tile (dinosaur)
+# 0000xxxx (available moves bitmask) (North=1, East=2, South=4, West=8)
+
 def left_shift(x, n):
     return x * (2 ** n)
 def right_shift(x, n):
@@ -57,7 +65,7 @@ def the_farmer_was_brainfucked(code):
     moves = {4:North,5:East,6:South,7:West}
     swap_moves = {32:North,33:East,34:South,35:West}
     plants_write = {128:Entities.Grass,129:Entities.Bush,130:Entities.Tree,131:Entities.Carrot,132:Entities.Pumpkin,133:Entities.Cactus,134:Entities.Sunflower}
-    plants_read = {None:0,Entities.Grass:1,Entities.Bush:2,Entities.Tree:3,Entities.Carrot:4,Entities.Pumpkin:5,Entities.Cactus:6,Entities.Sunflower:7}
+    plants_read = {None:0,Entities.Grass:1,Entities.Bush:2,Entities.Tree:3,Entities.Carrot:4,Entities.Pumpkin:5,Entities.Cactus:6,Entities.Sunflower:7,Entities.Dead_Pumpkin:8,Entities.Apple:9,Entities.Treasure:10,Entities.Hedge:11}
     items = {64:Items.Water,65:Items.Fertilizer,66:Items.Weird_Substance}
     plant_info = []
     info_ptr = 0
@@ -176,19 +184,48 @@ def the_farmer_was_brainfucked(code):
         elif code[ptr] == ',':
             if len(plant_info) == 0:
                 plant_type = get_entity_type()
-                water_level = get_water() * 4 // 1
-                if water_level == 4:
-                    water_level = 3
-                tilled = 0
-                if get_ground_type() == Grounds.Soil:
-                    tilled = 1
-                harvestable = 0
-                if can_harvest():
-                    harvestable = 1
-                water_level_shifted = left_shift(water_level,5)
-                plant_type_shifted = left_shift(plants_read[plant_type],2)
-                first_byte = water_level_shifted + plant_type_shifted + left_shift(tilled,1) + harvestable
-                plant_info.append(first_byte)
+                if plant_type == Entities.Hedge: #in mazes return chest coordinates and available moves
+                    x = get_pos_x()
+                    y = get_pos_y()
+                    moves = 0
+                    if can_move(North):
+                        moves += 1
+                    if can_move(East):
+                        moves += 2
+                    if can_move(South):
+                        moves += 4
+                    if can_move(West):
+                        moves += 8
+                    plant_info.append(left_shift(plants_read[plant_type],2))
+                    plant_info.append(x)
+                    plant_info.append(y)
+                    plant_info.append(moves)
+                elif plant_type == None: #when on an empty tile return move directions for use in dinosaur code
+                    plant_info.append(0)
+                    moves = 0
+                    if can_move(North):
+                        moves += 1
+                    if can_move(East):
+                        moves += 2
+                    if can_move(South):
+                        moves += 4
+                    if can_move(West):
+                        moves += 8
+                    plant_info.append(moves)
+                else:
+                    water_level = get_water() * 4 // 1
+                    if water_level == 4:
+                        water_level = 3
+                    tilled = 0
+                    if get_ground_type() == Grounds.Soil:
+                        tilled = 1
+                    harvestable = 0
+                    if can_harvest():
+                        harvestable = 1
+                    water_level_shifted = left_shift(water_level,6)
+                    plant_type_shifted = left_shift(plants_read[plant_type],2)
+                    first_byte = water_level_shifted + plant_type_shifted + left_shift(tilled,1) + harvestable
+                    plant_info.append(first_byte)
             elif len(plant_info) == 1:
                 plant_type = get_entity_type()
                 if plant_type in plants_with_companions:
